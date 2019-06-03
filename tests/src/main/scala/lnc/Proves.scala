5cc6d468de6ee749da9efb6fc347b1c81928e7ce
Marek Adamek <marek.jan.adamek@gmail.com>
2019-06-04 01:49:57 +0200

    daily

--- tests/src/main/scala/lnc/Proves.scala
+++ tests/src/main/scala/lnc/Proves.scala
@@ -11,47 +11,61 @@ object Proves extends App {
   import PrefixFormulaConverter._
   import lnc.expr.converters.NormalFormConverter._
 
-  val a = Var("a")
-  val b = Var("b")
-  val c = Var("c")
+  val p = Var("a")
+  val q = Var("b")
+  val r = Var("c")
+
+//  def toLatex(e: Expr): String = e match {
+//    case Var(p) => p
+//    case Not(x) => "\\lnot " + toLatex(x)
+//    case And(es) => "(" + es.map(toLatex).mkString(" \\land ") + ")"
+//    case Or(es) => "(" + es.map(toLatex).mkString(" \\lor ") + ")"
+//    case Impl(e1, e2) => "(" + toLatex(e1) + " \\leftarrow " + toLatex(e2) + ")"
+//    case Eq(e1, e2) => "(" + toLatex(e1) + " \\leftrightarrow " + toLatex(e2) + ")"
+//    case Next(e, l) => "N" + toLatex(e)
+//    case Change(e, l) =>"C" + toLatex(e)
+//  }
 
   val tautologies = List(
     // KRZ
-    a | !a
-    , a -> (a | b)
-    , (a & b) -> a
-    , ((a -> b) & (b -> c)) -> (a -> c)
-    , ((a -> c) & (b -> c)) -> ((a | b) -> c)
-    , ((a -> b) & (a -> c)) -> (a -> (b & c))
-    , ((a -> b) & (a -> !b)) -> !a
-    , ((a -> b) & a) -> b
-    , (a & (b | c)) -> ((a & b) | (a & c))
-    , !(a & b) -> (!a | !b)
+    p | !p
+    , p -> (p | q)
+    , (p & q) -> p
+    , ((p -> q) & (q -> r)) -> (p -> r)
+    , ((p -> r) & (q -> r)) -> ((p | q) -> r)
+    , ((p -> q) & (p -> r)) -> (p -> (q & r))
+    , ((p -> q) & (p -> !q)) -> !p
+    , ((p -> q) & p) -> q
+    , (p & (q | r)) -> ((p & q) | (p & r))
+    , !(p & q) -> (!p | !q)
 
     // LNC (axioms)
-    , !N(a) <-> N(!a)
-    , N(a -> b) <-> (N(a) -> N(b))
-    , C(a) <-> C(!a)
-    , C(a) -> (a -> N(!a))
-    , (a & N(!a)) -> C(a)
-    , (!a & N(a)) -> C(a)
-    , N(C(a)) <-> C(N(a))
+    , !N(p) <-> N(!p)
+    , N(p -> q) <-> (N(p) -> N(q))
+    , C(p) <-> C(!p)
+    , C(p) -> (p -> N(!p))
+    , (p & N(!p)) -> C(p)
+    , (!p & N(p)) -> C(p)
+    , N(C(p)) <-> C(N(p))
 
     // LNC (non-axioms)
-    , N(a) <-> (a <-> !C(a))
-    , C(a) <-> (a <-> !N(a))
-    , C(a & b) -> (C(a) | C(b))
-    , C(a <-> b) -> !(C(a) <-> C(b))
+    , N(p) <-> (p <-> !C(p))
+    , C(p) <-> (p <-> !N(p))
+    , C(p & q) -> (C(p) | C(q))
+    , C(p <-> q) -> !(C(p) <-> C(q))
   )
 
   val counterTautologies = List(
     //KRZ
-    (!a -> !b) <-> !(b -> a)
+    (!p -> !q) <-> !(q -> p)
 
     //LNC
-    , (!a -> !N(a)) <-> !(N(a) -> a)
-    , a & !C(a) & C(b) & !C(a -> b)
+    , (!p -> !N(p)) <-> !(N(p) -> p)
+    , p & !C(p) & C(q) & !C(p -> q)
   )
+
+  //tautologies.map(toLatex).foreach(println)
+  //counterTautologies.map(toLatex).foreach(println)
 
   val (result, tm) = time.measureTime {
     val tautologiesByCycleDetectionOk = tautologies.forall(t => {
